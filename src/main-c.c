91071809a92b42dad3c6feb7191ade6999a832f8
Spanti Nicola (RyDroid) <rydroid_dev@yahoo.com>
2014-12-18 19:41:17 +0100

    Managing players and creation of game board in text user interface

--- src/main-c.c
+++ src/main-c.c
@@ -26,7 +26,7 @@
 #include "stdio_functions.h"
 #include "tab_2d_char_io.h"
 #include "tab_2d_char_file.h"
-#include "halma_game_essential.h"
+#include "halma_game_board.h"
 
 #define USER_ANSWER_LENGTH_MAX 255
 
@@ -35,13 +35,15 @@ void print_help()
 {
   printf("h|help -- Print memo of commands.\n");
   printf("q|quit|k|kill|exit -- Quit the program.\n");
-  printf("l|load -- Load a board game.\n");
+  printf("new|start|begin -- Start a new Halma game party.\n");
+  printf("l|load -- Load a game board.\n");
   printf("n|next -- Compute next turn of the Halma game on the current board.\n");
-  printf("p|print|display -- Print the board game.\n");
+  printf("p|print|display -- Print the game board.\n");
   printf("nb_turns -- Print the current turn number.\n");
   printf("grid_size -- Print the size of the grid.\n");
   printf("nb_lines -- Print the number of lines.\n");
   printf("nb_columns -- Print the number of columns.\n");
+  printf("print_players -- Print the players.\n");
   printf("debug_status -- Print the status of debugging.\n");
   printf("debug_on -- Put debug on.\n");
   printf("debug_off -- Put debug off.\n");
@@ -52,26 +54,26 @@ void print_help()
  * @param universe A future universe
  * @return succeeded True if a universe is loaded, otherwise false
  */
-bool ask_load_board_game(tab_2d_char* board_game)
+bool ask_load_game_board(tab_2d_char* game_board)
 {
-  if(board_game == NULL)
+  if(game_board == NULL)
     {
-      fprintf(stderr, "ask_load_board_game => board_game == NULL\n");
+      fprintf(stderr, "ask_load_game_board => game_board == NULL\n");
       return false;
     }
   
-  tab_2d_char_destruct(board_game);
+  tab_2d_char_destruct(game_board);
   
-  printf("File path of the board game to load: ");
+  printf("File path of the game board to load: ");
   char file_path[2000];
   fgets(file_path, 2000, stdin);
   if(isspace(file_path[strlen(file_path)-1]) || iscntrl(file_path[strlen(file_path)-1]))
     file_path[strlen(file_path)-1] = '\0';
     
   if(strlen(file_path) == 0)
-    tab_2d_char_scan_stdin(board_game);
+    tab_2d_char_scan_stdin(game_board);
   else
-    tab_2d_char_get_from_file_path(board_game, file_path);
+    tab_2d_char_get_from_file_path(game_board, file_path);
   
   return true;
 }
@@ -110,7 +112,7 @@ int main(int argc, char* argv[])
   printf("Number of columns : ");
   scanf("%u", &number_of_columns);
   
-  tab_2d_char board_game = tab_2d_char_create(number_of_lines, number_of_columns);
+  tab_2d_char game_board = tab_2d_char_create(number_of_lines, number_of_columns);
   tab_2d_char_print_stdout_without_grid(&square);
   TODO Generate randomly*/
   
@@ -118,7 +120,8 @@ int main(int argc, char* argv[])
   puts("Use 'help' command if you need it.");
   puts("");
   
-  tab_2d_char board_game = tab_2d_char_create(0, 0);
+  tab_2d_char game_board = tab_2d_char_create(0, 0);
+  halma_game_players players = halma_game_players_create(0);
   char user_answer[USER_ANSWER_LENGTH_MAX] = "";
   unsigned int nb_turns = 0;
   
@@ -144,19 +147,77 @@ int main(int argc, char* argv[])
 	      string_equals(user_answer, "good bye")
 	 )
 	{
-	  tab_2d_char_destruct(&board_game);
+	  tab_2d_char_destruct(&game_board);
+	  halma_game_players_destruct(&players);
 	  return EXIT_SUCCESS;
+	}
+      else if(string_equals(user_answer, "start") || string_equals(user_answer, "begin") || string_equals(user_answer, "new") || string_equals(user_answer, "new_game"))
+	{
+	  nb_turns = 0;
+	  
+	  halma_game_players_destruct(&players);
+	  while(!halma_game_players_init(&players, players.nb))
+	    {
+	      printf("Number of players must be between %u and %u.\n", HALMA_GAME_NB_PLAYERS_MIN, HALMA_GAME_NB_PLAYERS_MAX);
+	      players.nb = ask_uint_tirelessly("Number of players: ", NULL);
+	    }
+	  
+	  {
+	    unsigned int nb_non_bots = players.nb;
+	    
+	    while(nb_non_bots >= players.nb)
+	      nb_non_bots = ask_uint_tirelessly("Number of non bot players: ", NULL);
+	    
+	    for(unsigned int i=0; i < players.nb; ++i)
+	      {
+		if(i < nb_non_bots)
+		  {
+		    do
+		      {
+			printf("Name of player %u: ", i);
+			fgets_trimmed(user_answer, USER_ANSWER_LENGTH_MAX, stdin);
+		      }
+		    while(!halma_game_player_set_name(&players.tab[i], user_answer));
+		    --nb_non_bots;
+		  }
+		else
+		  {
+		    sprintf(user_answer, "Bot %u", i);
+		    halma_game_player_set_name(&players.tab[i], user_answer); /* TODO verif pas 2 fois meme name */
+		  }
+	      }
+	  }
+	  
+	  if(!halma_game_players_set_char_pawns_with_numbers(&players))
+	    {
+	      unsigned int i=0;
+	      while(i < players.nb)
+		{
+		  printf("Choose a pawn character for player %u: ", i);
+		  fgets_trimmed(user_answer, USER_ANSWER_LENGTH_MAX, stdin);
+		  if(halma_game_players_set_player_char_pawn(&players, i, *user_answer))
+		    ++i;
+		  else
+		    fprintf(stderr, "The character '%c' is not authorized for this player.\n", *user_answer);
+		}
+	    }
+	  
+	  tab_2d_char_destruct(&game_board);
+	  game_board.nb_lines = 16;
+	  game_board.nb_columns = 16;
+	  if(!halma_game_board_construct_for_new_party(&game_board, &players))
+	    fprintf(stderr, "The game board was not successfully created. :(\n");
 	}
       else if(string_equals(user_answer, "l") || string_equals(user_answer, "load"))
 	{
-	  if(ask_load_board_game(&board_game))
+	  if(ask_load_game_board(&game_board))
 	    nb_turns = 0;
 	  else
-	    board_game.tab = NULL;
+	    game_board.tab = NULL;
 	}
       else if(string_equals(user_answer, "c") || string_equals(user_answer, "continue") || string_equals(user_answer, "n") || string_equals(user_answer, "next"))
 	{
-	  if(tab_2d_char_is_init(&board_game))
+	  if(tab_2d_char_is_init(&game_board))
 	    {
 	      unsigned int line_pawn, column_pawn, line_mark, column_mark;
 	      bool moved = false;
@@ -166,24 +227,24 @@ int main(int argc, char* argv[])
 		  line_pawn = ask_uint_tirelessly("* Line: ", NULL);
 		  column_pawn = ask_uint_tirelessly("* Column: ", NULL);
 		  
-		  if(halma_is_pawn(&board_game, line_pawn, column_pawn))
+		  if(halma_is_pawn(&game_board, line_pawn, column_pawn))
 		    {
-		      halma_mark_possible_moves_of_a_cell(&board_game, line_pawn, column_pawn);
-		      if(halma_is_there_at_least_one_mark(&board_game))
+		      halma_mark_possible_moves_of_a_cell(&game_board, line_pawn, column_pawn);
+		      if(halma_is_there_at_least_one_mark(&game_board))
 			{
 			  printf("Possible moves are marked with '%c'\n", HALMA_GAME_CELL_MARK);
-			  tab_2d_char_print_stdout_without_grid(&board_game);
+			  tab_2d_char_print_stdout_without_grid(&game_board);
 			  puts("Choose a destination cell:");
 			  line_mark = ask_uint_tirelessly("* Line: ", NULL);
 			  column_mark = ask_uint_tirelessly("* Column: ", NULL);
 			  
-			  if(halma_pawn_move(&board_game, line_pawn, column_pawn, line_mark, column_mark))
+			  if(halma_pawn_move(&game_board, line_pawn, column_pawn, line_mark, column_mark))
 			    {
 			      moved = true;
 			    }
 			  else
 			    {
-			      if(tab_2d_char_element_exists(&board_game, line_mark, column_mark))
+			      if(tab_2d_char_element_exists(&game_board, line_mark, column_mark))
 				fprintf(stderr, "The move did not succeed.\n");
 			      else
 				fprintf(stderr, "The cell [%u, %u] does not exist.\n", line_mark, column_mark);
@@ -191,7 +252,7 @@ int main(int argc, char* argv[])
 			}
 		      else
 			{
-			  if(tab_2d_char_element_exists(&board_game, line_pawn, column_pawn))
+			  if(tab_2d_char_element_exists(&game_board, line_pawn, column_pawn))
 			    fprintf(stderr, "The pawn [%u, %u] can not move.\n", line_pawn, column_pawn);
 			  else
 			    fprintf(stderr, "The cell [%u, %u] does not exist.\n", line_pawn, column_pawn);
@@ -208,10 +269,10 @@ int main(int argc, char* argv[])
 		{
 		  char file_path[6 + 1 + nb_turns / 1];
 		  sprintf(file_path, "debug-%u", nb_turns);
-		  tab_2d_char_save_to_file_path(&board_game, file_path);
+		  tab_2d_char_save_to_file_path(&game_board, file_path);
 		}
 
-	      /*if(halma_game_is_board_game_over(&board_game))
+	      /*if(halma_game_is_game_board_over(&game_board))
 		{
 		  user_answer[0] = 'o';
 		  user_answer[1] = 'v';
@@ -221,11 +282,11 @@ int main(int argc, char* argv[])
 		  }*/
 	    }
 	  else
-	    fprintf(stderr, "There is no board game. :(\n");
+	    fprintf(stderr, "There is no game board. :(\n");
 	}
       else if(string_equals(user_answer, "p") || string_equals(user_answer, "print") || string_equals(user_answer, "display"))
 	{
-	    tab_2d_char_print_stdout_without_grid(&board_game);
+	    tab_2d_char_print_stdout_without_grid(&game_board);
 	}
       else if(string_equals(user_answer, "nb_turns"))
 	{
@@ -233,24 +294,43 @@ int main(int argc, char* argv[])
 	}
       else if(string_equals(user_answer, "grid_size"))
 	{
-	  printf("%u", board_game.nb_lines);
+	  printf("%u", game_board.nb_lines);
 	  fputs(" line", stdout);
-	  if(board_game.nb_lines > 1)
+	  if(game_board.nb_lines > 1)
 	    fputc('s', stdout);
 	  fputs(" and ", stdout);
-	  printf("%u", board_game.nb_columns);
+	  printf("%u", game_board.nb_columns);
 	  fputs(" column", stdout);
-	  if(board_game.nb_columns > 1)
+	  if(game_board.nb_columns > 1)
 	    fputc('s', stdout);
 	  puts("");
 	}
       else if(string_equals(user_answer, "nb_lines"))
 	{
-	  printf("Number of lines: %u\n", board_game.nb_lines);
+	  printf("Number of lines: %u\n", game_board.nb_lines);
 	}
       else if(string_equals(user_answer, "nb_columns"))
 	{
-	  printf("Number of columns %u\n", board_game.nb_columns);
+	  printf("Number of columns %u\n", game_board.nb_columns);
+	}
+      else if(string_equals(user_answer, "print_players"))
+	{
+	  if(halma_game_players_is_init(&players))
+	    {
+	      for(unsigned int i=0; i < players.nb; ++i)
+		{
+		  printf("%u. %s (name), '%c' (pawn character), %c (ready)\n", i, players.tab[i].name, players.tab[i].char_pawn, halma_game_player_is_init(&players.tab[i]) ? 'y' : 'n');
+		}
+	      
+	      if(halma_game_players_is_correct(&players))
+		 printf("Players are corrects for playing.\n");
+	      else
+		 printf("Players are not corrects for playing.\n");
+	    }
+	  else
+	    {
+	      printf("Players are not initialized.\n");
+	    }
 	}
       else if(string_equals(user_answer, "debug_status"))
 	{
