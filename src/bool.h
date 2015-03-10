02f36c5aba1a367e21fe6ffa1c2ff76f92d602c1
Spanti Nicola (RyDroid) <rydroid_dev@yahoo.com>
2015-03-10 11:34:08 +0100

    For privacy, HTTPS matters

--- src/bool.h
+++ src/bool.h
@@ -7,7 +7,7 @@
  * License Creative Commons 0 (like the public domain)
  * Use, study, hack and share!
  * Even if I do not force you with copyleft to respect freedom of others, please do it.
- * For more informations : http://www.gnu.org/philosophy/free-sw.html
+ * For more informations : https://www.gnu.org/philosophy/free-sw.html
  * This program is provided with no warranty.
  * 
  * @section desc Description
